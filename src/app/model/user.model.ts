a308b02d56417ac6601007d8bd71fcab487399af
SUPITCHAYA_T\ADMIN <supitchaya1104gmail.com>
2021-12-25 21:58:52 +0700

    edit model

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -3,10 +3,29 @@ export class User {
     name:string;
     username:string;
     email:string;
-    address: Object;
+    address:Address;
     phone:string;
     website:string;
-    company:Object;
+    company:Company;
+}
+
+export class Address{
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo:Geo;
+}
+
+export class Geo{
+    lat: string;
+    lng: string;
+}
+
+export class Company{
+    name:string;
+    catchPhrase:string;
+    bs:string;
 }
 
 
--- src/app/pipe/search-filter.pipe.ts
+++ src/app/pipe/search-filter.pipe.ts
@@ -7,14 +7,18 @@ import { User } from '../model/user.model';
 export class SearchFilterPipe implements PipeTransform {
 
   transform(tableUsers:User[],inputSearch:string): User[] {
+    console.log(tableUsers);
+    console.log(inputSearch);
     if(!tableUsers||!inputSearch){
       return tableUsers;
     }
+
     return tableUsers.filter(user=>
       user.name.toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
       user.username.toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
       user.email.toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
       Object.values(user.address).toString().toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
+      Object.values(user.address.geo).toString().toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
       user.phone.toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
       user.website.toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase())||
       Object.values(user.company).toString().toLocaleLowerCase().includes(inputSearch.toLocaleLowerCase()));
