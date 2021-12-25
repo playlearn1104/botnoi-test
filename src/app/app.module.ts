6409621041f0af68905ee040606689737284943c
SUPITCHAYA_T\ADMIN <supitchaya1104gmail.com>
2021-12-25 19:38:02 +0700

    deploy gh page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TableUserComponent } from './components/table-user/table-user.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SearchFilterPipe } from './pipe/search-filter.pipe';
+import { ServiceService } from './services/service.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TableUserComponent } from './components/table-user/table-user.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,7 @@ import { SearchFilterPipe } from './pipe/search-filter.pipe';
     FormsModule
 
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
